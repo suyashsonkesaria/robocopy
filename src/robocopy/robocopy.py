0a668f647ba4c98c4fb71a815d1cc17f465f7ce5
suyashsonkesaria <suyashsonkesaria@gmail.com>
2023-07-02 20:50:04 +0530

    Updated robocopy.copy

--- src/robocopy/robocopy.py
+++ src/robocopy/robocopy.py
@@ -6,5 +6,3 @@ def copy(src,dst,options=''):
     print(result.stdout)
     if result.returncode==1:
         print(f"Copied files from {src} to {dst}")
-
-copy("E:\\suyash\\Project\\Python\\Python_Library\\robocopy","E:\\suyash\\Project\\Python\\testing library")
