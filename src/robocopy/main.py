064099cf00aae657f166520b4cb0c92a3afdc55a
suyashsonkesaria <suyashsonkesaria@gmail.com>
2023-07-02 18:07:18 +0530

    Updated robocopy.copy

--- src/robocopy/main.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def copy(src,dst):
-    command = f'robocopy {src} {dst} /E'
-    os.system(command)
-    print(f"Copied files from {src} to {dst}")
-
--- src/robocopy/robocopy.py
+++ src/robocopy/robocopy.py
@@ -1,7 +1,10 @@
-import os
+import subprocess
 
-def copy(src,dst):
-    command = f'robocopy {src} {dst} /E'
-    os.system(command)
-    print(f"Copied files from {src} to {dst}")
+def copy(src,dst,options=''):
+    command = f'robocopy "{src}" "{dst}" /E {options}'
+    result = subprocess.run(command,capture_output=True,text=True)
+    print(result.stdout)
+    if result.returncode==1:
+        print(f"Copied files from {src} to {dst}")
 
+copy("E:\\suyash\\Project\\Python\\Python_Library\\robocopy","E:\\suyash\\Project\\Python\\testing library")
